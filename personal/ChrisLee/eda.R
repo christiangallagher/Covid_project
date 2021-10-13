8f18268743e3f286f46c4cada4394c95c7c618b9
Chris Lee <chreestopherlee@gmail.com>
2021-10-13 15:55:17 -0400

    Changes - CL

--- personal/ChrisLee/eda.R
+++ personal/ChrisLee/eda.R
@@ -75,3 +75,22 @@ chipotle_in_county <- st_join(dat, cal, join = st_within)
 chipotle_in_county %>%
     as_tibble() %>%
     count(geoid, name)
+
+ksu <- tibble(latitude = 34.037876, longitude = -84.58102) %>%
+    st_as_sf(coords = c("longitude", "latitude"), crs = 4326)
+
+# https://epsg.io/4326 units are degrees
+calw <- cal %>%
+    st_transform(3310) %>% # search https://spatialreference.org/ref/?search=california&srtext=Search. Units are in meters for buffer.
+    filter(name != "San Francisco") %>%
+    mutate(
+        aland_acres = aland * 0.000247105,
+        awater_acres = awater * 0.000247105,
+        percent_water = 100 * (awater / aland),
+        sf_area = st_area(geometry),
+        sf_center = st_centroid(geometry),
+        sf_length = st_length(geometry),
+        sf_distance = st_distance(sf_center, ksu),
+        sf_buffer = st_buffer(sf_center, 24140.2), # 24140.2 is 15 miles
+        sf_intersects = st_intersects(., filter(., name == "Los Angeles"), sparse = FALSE)
+        )
