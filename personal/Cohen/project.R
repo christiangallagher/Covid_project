a91f598359e0c98c0daaaedec769b50931552afb
L. Cohen <ltcohen43@gmail.com>
2021-10-18 14:29:05 -0400

    progress

--- personal/Cohen/project.R
+++ personal/Cohen/project.R
@@ -8,6 +8,10 @@ july <- read_csv("Data folder/core_poi-patterns_07_2021.csv")
 aug <- read_csv("Data folder/core_poi-patterns_08_2021.csv")
 sep <- read_csv("Data folder/core_poi-patterns_09_2021.csv")
 
+dat <- rbind(july, aug, sep)
+
+dat <- dat %>% filter(region == "GA")
+
 dat_aug <- aug %>% st_as_sf(coords = c("longitude", "latitude"), crs = 4326)
 
 ga <- us_counties(states = "Georgia") %>%
@@ -20,11 +24,10 @@ ga %>%
     select(name, states_area, aland, sf_area, awater)
 
 ksu <- tibble(latitude = 34.037876, longitude = -84.58102) %>%
-    st_as_sf(coords = c("longitude", "latitude"), crs = 3310)
-
+    st_as_sf(coords = c("longitude", "latitude"), crs = 4326)
 
 gaw <- ga %>%
-    st_transform(3310) %>%
+    st_transform(4326) %>%
     mutate(
         aland_acres = aland * 0.000247105,
         awater_acres = awater * 0.000247105,
@@ -40,14 +43,14 @@ ggplot(data = gaw) +
     geom_sf(aes(geometry = sf_buffer), fill = "white") +
     geom_sf(aes(geometry = sf_center), color = "darkgrey") +
     geom_sf_text(aes(label = name), color = "lightgrey") +
-    geom_sf(data = filter(dat, region == "GA"), color = "black") +
+    geom_sf(data = filter(dat_aug, region == "GA"), color = "black") +
     theme_bw()
 
 ggplot(data = gaw) +
     geom_sf(aes(geometry = sf_buffer), fill = NA) +
     geom_sf(aes(geometry = sf_center), color = "darkgrey") +
     geom_sf_text(aes(label = name), color = "lightgrey") +
-    geom_sf(data = filter(dat, region == "GA"), color = "black") + 
+    geom_sf(data = filter(dat_aug, region == "GA"), color = "black") +
     theme_bw()
 
 covid_cases <- st_join(dat_aug, ga, join = st_within) %>%
@@ -65,7 +68,7 @@ gaw %>%
 ggplot() +
     geom_sf(aes(fill = n)) +
     scale_fill_continuous(trans = "sqrt") +
-    geom_sf(data = filter(dat, region == "GA"), color = "white", shape = "x") +
+    geom_sf(data = filter(dat_aug, region == "GA"), color = "white", shape = "x") + # nolint
     theme_bw() +
     theme(legend.position = "bottom") +
     labs(fill = "Number of Hospitals")
