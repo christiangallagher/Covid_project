83812de2c0b9f34a40da93c1808a009aecbb658c
L. Cohen <ltcohen43@gmail.com>
2021-10-25 16:23:10 -0400

    class

--- personal/Cohen/project.R
+++ personal/Cohen/project.R
@@ -1,4 +1,4 @@
-pacman::p_load(tidyverse, sf, USAboundaries, leaflet, cowplot)
+pacman::p_load(tidyverse, sf, USAboundaries, leaflet, cowplot, stringdist)
 #install.packages("USAboundariesData", repos = "http://packages.ropensci.org", type = "source") #nolint
 
 httpgd::hgd()
@@ -34,8 +34,7 @@ dat <- dat %>%
     visitor_daytime_cbgs = map(visitor_daytime_cbgs, ~json_to_tibble(.x)),
   )
 
-ga <- us_counties(states = "Georgia") %>%
-    select(countyfp, countyns, name, aland, awater, state_abbr, geometry)
+ga <- USAboundaries::us_counties(states = "Georgia")
 
 ggplot() +
     geom_sf(data = ga) +
@@ -68,19 +67,19 @@ gaw <- ga %>%
         sf_buffer = st_buffer(sf_center, 24140.2), # 24140.2 is 15 miles
     )
 
-ggplot(data = gaw) +
-    geom_sf(aes(geometry = sf_buffer), fill = "white") +
-    geom_sf(aes(geometry = sf_center), color = "darkgrey") +
-    geom_sf_text(aes(label = name), color = "lightgrey") +
-    geom_sf(data = filter(dat_july, region == "GA"), color = "black") +
-    theme_bw()
+# ggplot(data = gaw) +
+#     geom_sf(aes(geometry = sf_buffer), fill = "white") +
+#     geom_sf(aes(geometry = sf_center), color = "darkgrey") +
+#     geom_sf_text(aes(label = name), color = "lightgrey") +
+#     geom_sf(data = filter(dat_july, region == "GA"), color = "black") +
+#     theme_bw()
 
-ggplot(data = gaw) +
-    geom_sf(aes(geometry = sf_buffer), fill = NA) +
-    geom_sf(aes(geometry = sf_center), color = "darkgrey") +
-    geom_sf_text(aes(label = name), color = "lightgrey") +
-    geom_sf(data = filter(dat_july, region == "GA"), color = "black") +
-    theme_bw()
+# ggplot(data = gaw) +
+#     geom_sf(aes(geometry = sf_buffer), fill = NA) +
+#     geom_sf(aes(geometry = sf_center), color = "darkgrey") +
+#     geom_sf_text(aes(label = name), color = "lightgrey") +
+#     geom_sf(data = filter(dat_july, region == "GA"), color = "black") +
+#     theme_bw()
 
 covid_cases <- st_join(dat_july, ga, join = st_within) %>%
     select(placekey, visits_by_day, city, region, geometry, raw_visitor_counts, location_name, countyfp) # nolint
@@ -93,37 +92,37 @@ covid_cases_count <- covid_cases %>%
 gaw <- gaw %>%
     left_join(covid_cases_count, fill = 0)
 
-aug_plot <- gaw %>%
-ggplot() +
-    geom_sf(aes(fill = n)) +
-    scale_fill_continuous(trans = "sqrt") +
-    geom_sf(data = filter(dat_aug, region == "GA"), color = "white", shape = "x") + # nolint
-    theme_bw() +
-    theme(legend.position = "bottom") +
-    labs(fill = "Number of Hospitals")
+# aug_plot <- gaw %>%
+# ggplot() +
+#     geom_sf(aes(fill = n)) +
+#     scale_fill_continuous(trans = "sqrt") +
+#     geom_sf(data = filter(dat_aug, region == "GA"), color = "white", shape = "x") + # nolint
+#     theme_bw() +
+#     theme(legend.position = "bottom") +
+#     labs(fill = "Number of Hospitals")
 
-july_plot <- gaw %>%
-ggplot() +
-    geom_sf(aes(fill = n)) +
-    scale_fill_continuous(trans = "sqrt") +
-    geom_sf(data = filter(dat_july, region == "GA"), color = "white", shape = "x") + # nolint
-    theme_bw() +
-    theme(legend.position = "bottom") +
-    labs(fill = "Number of Hospitals")
+# july_plot <- gaw %>%
+# ggplot() +
+#     geom_sf(aes(fill = n)) +
+#     scale_fill_continuous(trans = "sqrt") +
+#     geom_sf(data = filter(dat_july, region == "GA"), color = "white", shape = "x") + # nolint
+#     theme_bw() +
+#     theme(legend.position = "bottom") +
+#     labs(fill = "Number of Hospitals")
 
-sep_plot <- gaw %>%
-ggplot() +
-    geom_sf(aes(fill = n)) +
-    scale_fill_continuous(trans = "sqrt") +
-    geom_sf(data = filter(dat_sep, region == "GA"), color = "white", shape = "x") + # nolint
-    theme_bw() +
-    theme(legend.position = "bottom") +
-    labs(fill = "Number of Hospitals")
+# sep_plot <- gaw %>%
+# ggplot() +
+#     geom_sf(aes(fill = n)) +
+#     scale_fill_continuous(trans = "sqrt") +
+#     geom_sf(data = filter(dat_sep, region == "GA"), color = "white", shape = "x") + # nolint
+#     theme_bw() +
+#     theme(legend.position = "bottom") +
+#     labs(fill = "Number of Hospitals")
 
-ggdraw() +
-  draw_plot(july_plot, x = 0, y = .5, width = .5, height = .5) +
-  draw_plot(sep_plot, x = .5, y = .5, width = .5, height = .5) +
-  draw_plot(aug_plot, x = .5, y = .5, width = .5, height = .5)
+# ggdraw() +
+#   draw_plot(july_plot, x = 0, y = .5, width = .5, height = .5) +
+#   draw_plot(sep_plot, x = .5, y = .5, width = .5, height = .5) +
+#   draw_plot(aug_plot, x = .5, y = .5, width = .5, height = .5)
 
 # all the months on the plot
 
@@ -142,3 +141,41 @@ ggplot() +
     theme_bw() +
     theme(legend.position = "bottom") +
     labs(fill = "Number of Hospitals")
+
+
+sites <- read.csv("Data folder/WalgreensSites.csv")
+
+glimpse(sites)
+cases <- read.csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/rolling-averages/us-counties-2021.csv") # nolint
+
+cases <- cases %>% filter(state == "Georgia", date >= as.Date("2021-07-01"), date <= as.Date("2021-09-30")) # nolint
+
+cases %>%
+ggplot() +
+    geom_sf(data = ga) +
+    theme_bw()
+
+ga <- ga[, !duplicated(colnames(ga))]
+
+colnames(ga)[6] <- "county"
+
+casga <- merge(cases, ga, by = "county")
+
+test1 <- casga  %>%
+group_by(county) %>%
+summarise(pcc = sum(cases_avg_per_100k))
+
+countycases <- merge(ga, test1, by = "county")
+
+ggplot() +
+    geom_sf(data = countycases, aes(fill = pcc)) +
+    scale_fill_viridis_c(trans = "sqrt", alpha = .4) +
+    geom_sf(data = dat_sf)
+
+#Do cases correlate with number of testing sites in the county?
+#Do cases correlate with number of hospital visits?
+#How do we even begin plotting by date?
+# I guess we don't really do sf, but more just temporal plots
+#So many lingering questions
+
+allsites <- read_csv("Data folder/Covid_Testing_Sites.csv")
